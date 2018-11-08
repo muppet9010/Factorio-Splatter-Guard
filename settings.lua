799411109b34b37fc769d3dd51ad7dbdd1f4bef0
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2018-11-08 17:18:46 +0000

    test

---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+	{
+		name = "train-avoid-mode",
+		type = "string-setting",
+		default_value = "Preemtive",
+		allowed_values = {"Preemtive", "Reactionary", "None"},
+		setting_type = "runtime-global",
+		order = "1001"
+	}
+})
